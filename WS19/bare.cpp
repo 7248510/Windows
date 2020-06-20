c9334bd82a4ce0a0058e58e6993dfb73ced7eea4
7248510 <calebkre19@gmail.com>
2020-06-19 22:47:01 -0400

    Code cleanup, duplicated Virtual

---
+++ WS19/bare.cpp
@@ -0,0 +1,45 @@
+#include <windows.h>
+#include <iostream>
+#include <cstdio>
+//The binary is 117kb
+//Designed for a workstation, I cycle between remote desktop panels or SSH
+//Possible implementations:
+//Identify the OS to determine the which function to run.(Aiming for cross compatibly between Linux & Windows, I don't use Mac)(Start learning Linux's api)
+//Sign my code
+
+//This is the Unique Identification structure. Virtualbox hashes machines.
+struct bvirt {
+    char uid[150];
+};
+//Command line arguments, vboxExec's string needs the path of the VirtualBox installation location
+struct cmdargs {
+    std::string vboxExec = "C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage.exe startvm";
+    std::string vboxType = "--type headless";
+};
+void launch(bvirt* uid_ptr) //passing the UID pointer to the function, it gets the pointer from vboxExecute()
+{
+    cmdargs main; //initializing the structure command arguments
+    std::string vboxMain, vboxHeadless, combinationVbox, uidPointer, space; //Declaring strings
+    vboxMain =  main.vboxExec;
+    vboxHeadless = main.vboxType;
+    uidPointer = uid_ptr->uid;
+    combinationVbox = main.vboxExec + main.vboxType;
+    space = " ";
+    //Adding an extra space caused a logical error(the command wouldn't execute)
+    std::string finalCommand = vboxMain + space + uidPointer + space + vboxHeadless;
+    const char *finalp = finalCommand.c_str();
+    //Executing the finalp, WinExec required a const char *, converting a string to a pointer is the [name].c_str()
+    WinExec(finalp,0);
+}
+int vboxExecute()
+{
+    //bvirt is your UID structure, simply declare an array and pass the UID, it passes the value as a pointer to launch, once launch gets the pointer it passes it to WinExec
+    bvirt centos[] = {"09b3289f-42ae-4578-ae12-122accd3bcbf"}; //Local machine
+    //Starting a centOS machine
+    launch(centos);
+    return 0;
+}
+int main()
+{
+    vboxExecute();
+}
