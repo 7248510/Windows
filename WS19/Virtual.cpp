ddcb9aced22068fbb49e71b1ed213c5addfa648d
Caleb <56181475+7248510@users.noreply.github.com>
2021-09-18 09:50:16 -0400

    Update Virtual.cpp
    Remove comments. This program does not use the Window's API's recommended way of creating a process(createprocess) instead it used WinExec.

--- WS19/Virtual.cpp
+++ WS19/Virtual.cpp
@@ -18,40 +18,20 @@ void launch(bvirt* uid_ptr) //passing the UID pointer to the function, it gets t
     uidPointer = uid_ptr->uid;
     combinationVbox = main.vboxExec + main.vboxType;
     space = " ";
-    //printf("%s %s %s", main.base,uid_ptr->uid,main.headless);
-    //std::string vboxDir = "C:\\Program Files\\Oracle\\VirtualBox\\";
-    //Adding an extra space caused a logical error(the command wouldn't execute)
     std::string finalCommand = vboxMain + space + uidPointer + space + vboxHeadless;
-    //std::string finalArgs = uidPointer + space + vboxHeadless;
     const char *finalp = finalCommand.c_str();
-    //const char *finalArgsp = finalArgs.c_str();
-    //std::cout << finalp << "\n";
-    //Executing the finalp, WinExec required a const char *, converting a string to a pointer is the [name].c_str()
-    //printf("%s",finalp);
     WinExec(finalp,0);
-    //const char *final2 = test.c_str();
-    //WinExec(final2,0);
-    //printf("%s\n", finalp);
-    //std::cout << finalCommand << "\n";
-    //std::cout << "" << finalArgsp << ")\n"; //Making sure the arguments pass 
-    //SetCurrentDirectory(finalDir);
-    //WinExec();
-    //ShellExecute(NULL,"open",finalp,finalArgsp,NULL,0);
 }
 int vboxExecute()
 {
     //bvirt is your UID structure, simply declare an array and pass the UID, it passes the value as a pointer to launch, once launch gets the pointer it passes it to WinExec
     bvirt uidMetasploitableW32[] = {"a005fd05-8305-438e-acc0-4e6dfea5ab8a"}; //Metasploitable3 Windows 
     bvirt uidMetasploitableUB[] = {"99bb87b5-beb6-4fd5-8e95-29c40c083e28"}; //Metasploitable3 Ubuntu
-    bvirt w10[] = {"eee497e6-a22b-4ec2-b688-158dd812e65a"}; //I'll probably have to change the uid, the 90 days most likely expired
+    bvirt w10[] = {"eee497e6-a22b-4ec2-b688-158dd812e65a"}; //Change the uid, the 90 days VM expired
     bvirt w19[] = {"2df0931b-5bf9-46c3-978c-b692b79ad975"}; //Windows server
     bvirt gitLab[] = {"47e6ac85-92c2-4497-a177-1816dce47579"}; //Gitlab enterprise/community offline
     bvirt pfsenseBridge[] = {"1f82b7e4-0812-4a47-a716-b6ccfa718b6c"}; //pfsense bridged
     bvirt pfsenseInternal[] = {"b7f8d618-7382-4483-af93-865b8ce8bbe9"}; //internal pfsense
-    //bvirt debian[] = {"683cda2a-1307-467a-91af-18dcbbb592c4"}; //Local machine
-    //bvirt [] = {""}; 
-    //bvirt [] = {""};
-    //bvirt [] = {""};
     launch(uidMetasploitableW32);
     launch(uidMetasploitableUB);
     launch(w10);
@@ -59,16 +39,9 @@ int vboxExecute()
     launch(gitLab);
     launch(pfsenseBridge);
     launch(pfsenseInternal);
-    //launch(debian);
-    //launch();
-    //launch();
-    //launch();
     return 0;
 }
 int main()
 {
     vboxExecute();
-    //ShellExecute(NULL,"open","C:\\Users\\Caleb\\AppData\\Local\\Discord\\update.exe","parameters",NULL,10);
-    //ShellExecute(NULL,"open","C:\\Users\\Caleb\\Downloads\\VirtualBox-6.1.10-138449-Win.exe",NULL,NULL,0);
-    //WinExec("E:\\meta\\Vbox\\VboxManage.exe startvm 683cda2a-1307-467a-91af-18dcbbb592c4 --type headless", 0);
 }
